cfd8a5cbfbeab480ea36417c966723b471dbc10e
enjsite <enjyweb@gmail.com>
2025-03-30 21:38:18 +0300

    project structure

---
+++ src/main/java/ru/enjy/fit_balance/FitBalanceApplication.java
@@ -0,0 +1,13 @@
+package ru.enjy.fit_balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitBalanceApplication.class, args);
+	}
+
+}
